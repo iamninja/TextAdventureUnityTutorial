e3c928ca214694e029c4966c89ebc228db2f622d
Vagios Vlachos <bagios234@gmail.com>
2017-09-03 17:51:37 +0300

    Preparing the use item dictionary

--- Assets/Scripts/InteractableItems.cs
+++ Assets/Scripts/InteractableItems.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class InteractableItems : MonoBehaviour
 {
+    public List<InteractableObject> usableItemList;
     public Dictionary<string, string> examineDictionary = new Dictionary<string, string>();
     public Dictionary<string, string> takeDictionary = new Dictionary<string, string>();
 
     [HideInInspector] public List<string> nounsInRoom = new List<string>();
 
+    private Dictionary<string, ActionResponse> useDictionary = new Dictionary<string, ActionResponse>();
     private List<string> nounsInInventory = new List<string>();
     private GameController controller;
 
@@ -27,6 +29,43 @@ public class InteractableItems : MonoBehaviour
             return interactableInRoom.description;
         }
 
+        return null;
+    }
+
+    public void AddActionResponsesToUseDictionary()
+    {
+        for (int i = 0; i < nounsInInventory.Count; i++)
+        {
+            string noun = nounsInInventory[i];
+
+            InteractableObject interactableObjectInInventory = GetInteractableObjectFromUsableList(noun);
+            if (interactableObjectInInventory == null)
+                continue;
+
+            for (int j = 0; j < interactableObjectInInventory.interactions.Length; j++)
+            {
+                Interaction interaction = interactableObjectInInventory.interactions[j];
+
+                if (interaction.actionResponse == null)
+                    continue;
+
+                if (!useDictionary.ContainsKey(noun))
+                {
+                    useDictionary.Add(noun, interaction.actionResponse);
+                }
+            }
+        }
+    }
+
+    private InteractableObject GetInteractableObjectFromUsableList(string noun)
+    {
+        for (int i = 0; i < usableItemList.Count; i++)
+        {
+            if (usableItemList[i].noun == noun)
+            {
+                return usableItemList[i];
+            }
+        }
         return null;
     }
 
@@ -53,6 +92,7 @@ public class InteractableItems : MonoBehaviour
         if (nounsInRoom.Contains(noun))
         {
             nounsInInventory.Add(noun);
+            AddActionResponsesToUseDictionary();
             nounsInRoom.Remove(noun);
             return takeDictionary;
         }
