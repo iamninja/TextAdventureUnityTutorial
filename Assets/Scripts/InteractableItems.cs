20685f1853c31c79a0c915e8a82a183d674a879a
Vagios Vlachos <bagios234@gmail.com>
2017-09-03 17:28:07 +0300

    Displaying inventory

--- Assets/Scripts/InteractableItems.cs
+++ Assets/Scripts/InteractableItems.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class InteractableItems : MonoBehaviour
 {
     public Dictionary<string, string> examineDictionary = new Dictionary<string, string>();
+    public Dictionary<string, string> takeDictionary = new Dictionary<string, string>();
 
     [HideInInspector] public List<string> nounsInRoom = new List<string>();
+
     private List<string> nounsInInventory = new List<string>();
+    private GameController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<GameController>();
+    }
 
     public string GetObjectsNotInInventory(Room currentRoom, int i)
     {
@@ -21,10 +29,38 @@ public class InteractableItems : MonoBehaviour
 
         return null;
     }
+
+    public void DisplayInventory()
+    {
+        controller.LogStringWithReturn("You look in your backpack. Inside you have: ");
+        for (int i = 0; i < nounsInInventory.Count; i++)
+        {
+            controller.LogStringWithReturn(nounsInInventory[i]);
+        }
+    }
 	
     public void ClearCollections()
     {
         examineDictionary.Clear();
+        takeDictionary.Clear();
         nounsInRoom.Clear();
+    }
+
+    public Dictionary<string, string> Take (string[] separatedInputWords)
+    {
+        string noun = separatedInputWords[1];
+
+        if (nounsInRoom.Contains(noun))
+        {
+            nounsInInventory.Add(noun);
+            nounsInRoom.Remove(noun);
+            return takeDictionary;
+        }
+        else
+        {
+            controller.LogStringWithReturn("There is no " + noun + " here to take.");
+            return null;
+        }
+
     }
 }
